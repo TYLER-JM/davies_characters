3d4aa52f4a3a2b175152dbcf739b54428ad701d2
tyler martin <t.mar92@gmail.com>
2020-02-22 13:40:01 -0500

    adds method to handle adding characters and novels to the database

--- application/controllers/Characters.php
+++ application/controllers/Characters.php
@@ -1,4 +1,9 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, OPTIONS, POST");
+header('Access-Control-Allow-Headers: Content-Type');
+?>
+<?php
 
 Class Characters extends CI_Controller {
 	public function __construct()
@@ -6,6 +11,10 @@ Class Characters extends CI_Controller {
 		parent::__construct();
 		$this->load->model('characters_model');
 		$this->load->helper('url_helper');
+
+		$this->output->set_header('Access-Control-Allow-Origin: http://localhost:8080');
+		$this->output->set_header('Access-Control-Allow-Methods: GET, OPTIONS, DELETE, POST');
+		$this->output->set_header('Access-Control-Allow-Headers: Content-Type');
 	}
 
 	public function index()
@@ -14,5 +23,14 @@ Class Characters extends CI_Controller {
 		$this->output
 							->set_content_type('application/json')
 							->set_output(json_encode($data['characters']));
+	}
+
+	public function add()
+	{
+		$json_data = json_decode(file_get_contents('php://input'), true);
+		// $this->output
+		// 					->set_content_type('application/json')
+		// 					->set_output(json_encode($json_data));
+		$this->characters_model->add_character($json_data);
 	}
 }
--- application/models/Characters_model.php
+++ application/models/Characters_model.php
@@ -5,11 +5,45 @@ Class Characters_model extends CI_model {
 	{
 		$this->load->database();
 	}
+
 	public function get_characters($slug = false)
 	{
-		if ($slug === false) {
+		if ($slug === false)
+		{
 			$query = $this->db->get('characters');
 			return $query->result_array();
+		}
+	}
+
+	public function add_character($data)
+	{
+		$novel_data = array(
+			'title' => $data['novel'],
+			'year_published' => $data['year_published']
+		);
+		# check if the Novel exists in the Database Already
+		# ...I should also format the text (Capitalize each first word)
+		$check_query = $this->db->get_where('novels', $novel_data);
+		$row = $check_query->row();
+		if (! $row)
+		{
+			$this->db->insert('novels', $novel_data);
+			$novel_query = $this->db->get_where('novels', $novel_data);
+			$row = $novel_query->row();
+		}
+
+		$character_data = array(
+			'first_name' => $data['first_name'],
+			'last_name' => $data['last_name'],
+			'novel_id' => $row->id
+		);
+
+		# only insert if the character doesn't already exist
+		$character_query = $this->db->get_where('characters', $character_data);
+		$row = $character_query->row();
+		if (! $row)
+		{
+			$this->db->insert('characters', $character_data);
 		}
 	}
 }
