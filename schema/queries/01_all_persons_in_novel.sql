1d8b93eb36bf0ce959718b2966ab13d6f421d12b
tyler martin <t.mar92@gmail.com>
2020-02-25 15:44:11 -0500

    changes one of the sample queries

--- schema/queries/01_all_persons_in_novel.sql
+++ schema/queries/01_all_persons_in_novel.sql
@@ -2,4 +2,4 @@ SELECT person.first_name, person.last_name
 FROM person
 join person_novel ON person.id = person_novel.person_id
 join novel ON novel.id = person_novel.novel_id
-WHERE novel.title = 'Tempest Tost';
+WHERE novel.title = 'Fifth Business';
