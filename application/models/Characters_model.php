1b71ba75187be9f014246980cd00fe9806031d47
tyler martin <t.mar92@gmail.com>
2020-02-25 14:20:28 -0500

    uses the OR operator to set values of data to be inserted into db

--- application/models/Characters_model.php
+++ application/models/Characters_model.php
@@ -23,25 +23,51 @@ Class Characters_model extends CI_model {
 		);
 		# check if the Novel exists in the Database Already
 		$check_query = $this->db->get_where('novel', $novel_data);
-		$row = $check_query->row();
-		if (! $row)
+		$novel = $check_query->row();
+		if (! $novel)
 		{
 			$this->db->insert('novels', $novel_data);
 			$novel_query = $this->db->get_where('novel', $novel_data);
-			$row = $novel_query->row();
+			$novel = $novel_query->row();
 		}
 
-		$character_data = array(
-			'first_name' => ucwords(strtolower(trim($data['first_name']))),
-			'last_name' => ucwords(strtolower(trim($data['last_name'])))
-		);
+		// $character_data = array(
+		// 	'first_name' => ucwords(strtolower(trim($data['first_name']))),
+		// 	'last_name' => ucwords(strtolower(trim($data['last_name']))),
+		// 	'birth_name' => ucwords(strtolower(trim($data['birth_name']))),
+		// 	'nick_name' => ucwords(strtolower(trim($data['nick_name']))),
+		// 	'hometown' => ucwords(strtolower(trim($data['hometown']))),
+		// 	'about' => strtolower(trim($data['about'])),
+		// 	'birth_year' => trim($data['birth_year']),
+		// 	'death_year' => trim($data['death_year']),
+		// );
+
+		$character_data['first_name'] = ucwords(strtolower(trim($data['first_name'])));
+		$character_data['last_name'] = ucwords(strtolower(trim($data['last_name'])));
 
 		# only insert if the character doesn't already exist
 		$character_query = $this->db->get_where('person', $character_data);
-		$row = $character_query->row();
-		if (! $row)
+		$new_char = $character_query->row();
+		if (! $new_char)
 		{
+			$character_data['birth_name'] = ucwords(strtolower(trim($data['birth_name']))) || null;
+			$character_data['nick_name'] = ucwords(strtolower(trim($data['nick_name']))) || null;
+			$character_data['hometown'] = ucwords(strtolower(trim($data['hometown']))) || null;
+			$character_data['about'] = strtolower(trim($data['about'])) || null;
+			$character_data['birth_year'] = trim($data['birth_year']) || null;
+			$character_data['death_year'] = trim($data['death_year']) || null;
+
 			$this->db->insert('person', $character_data);
+
+			$new_character_query = $this->db->get_where('person', $character_data);
+			$new_char = $new_character_query->row();
+
+			$new_join = array(
+				'novel_id' => $novel->id,
+				'person_id' => $new_char->id
+			);
+
+			$this->db->insert('person_novel', $new_join);
 		}
 	}
 }
